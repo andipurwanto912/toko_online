f9204513fe31b3ade0347752a2e83521a8d89efd
Andip114 <andipurwanto912@gmail.com>
2020-05-13 23:34:54 +0700

    forgot password

--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -73,7 +73,7 @@
                 <ul class="polished-sidebar-menu ml-0 pt-4 p-0 d-md-block">
                     <input class="border-dark form-control d-block d-md-none mb-4" type="text" placeholder="Search"
                         aria-label="Search" />
-                    <li><a href="/home"><span class="oi oi-home"></span>Home</a></li>
+                    <li><a href="#"><span class="oi oi-home"></span>Home</a></li>
                     <li>
                         <a href="{{route('users.index')}}">
                             <span class="oi oi-people"></span> Manage Users
